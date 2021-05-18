1b98de1bfa58aafeed5a5d0dff61dc764eed64da
JD <jann.driessen@gmx.de>
2021-05-18 19:49:05 +0200

    Add header

--- unigraph/ContentView.swift
+++ unigraph/ContentView.swift
@@ -7,10 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TokenPairList {
+    let id = UUID()
+    let selectedColor: Color
+    let title: String
+}
+
+extension TokenPairList {
+    static var lists: [TokenPairList] {
+        return [
+            TokenPairList(selectedColor: .blue, title: "Pool size"),
+            TokenPairList(selectedColor: .green, title: "Annualized fees"),
+            TokenPairList(selectedColor: .green, title: "30d profit"),
+            TokenPairList(selectedColor: .purple, title: "24h volume"),
+        ]
+    }
+}
+
+struct FilterView: View {
+    var arrowUp: Bool
+    var selected: Bool
+    var selectedColor: Color
+    var title: String
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let highlightColor = selected ? selectedColor : .gray
+        
+        return (
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(selected ? .gray.opacity(0.1) : Color.white)
+                HStack {
+                    Text(title)
+                        .bold()
+                        .foregroundColor(highlightColor)
+                        .font(.caption)
+                    if selected {
+                        Image(systemName: arrowUp ? "arrow.up" : "arrow.down")
+                            .font(Font.system(.caption).bold())
+                            .foregroundColor(selectedColor)
+                            .padding(.leading, -4)
+                    }
+                }
+                .padding(8)
+            }
+        )
+    }
+}
+
+struct ContentView: View {
+    private var lists = TokenPairList.lists
+    @State private var ascending: Bool = false
+    @State private var selectedListId: UUID?
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Uniswap Pools")
+                        .bold()
+                        .padding()
+                    Spacer()
+                }
+                .background(Color.purple)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        Spacer()
+                        ForEach(lists, id: \.id) { list in
+                            FilterView(arrowUp: ascending, selected: selectedListId == list.id, selectedColor: list.selectedColor, title: list.title)
+                                .animation(.spring())
+                                .onTapGesture {
+                                    let feedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+                                    feedbackGenerator.impactOccurred()
+                                    if selectedListId == list.id {
+                                        self.ascending.toggle()
+                                        return
+                                    }
+                                    self.ascending = false
+                                    self.selectedListId = list.id
+                                }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
