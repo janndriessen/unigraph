ff4b7618aaff56c8a022a90494c452c6c158ffcf
JD <jann.driessen@gmx.de>
2021-05-18 18:35:17 +0200

    Initial Commit

---
+++ unigraph/unigraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  unigraphApp.swift
+//  unigraph
+//
+//  Created by Jann Driessen on 18.05.21.
+//
+
+import SwiftUI
+
+@main
+struct unigraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
